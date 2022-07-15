7d404d92884d5b0f38edc10938bcd93b66469224
Andrea <mirthis@gmail.com>
2022-07-15 14:46:53 +0200

    add mine and mint scripts

---
+++ scripts/mine.js
@@ -0,0 +1,14 @@
+const { moveBlocks } = require("../utils/moveBlocks");
+
+const mine = async () => {
+  if ((network.config.chainId = 31337)) {
+    await moveBlocks(2, 1000);
+  }
+};
+
+mine()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
